507014b01274e68511a1aa30d9d1dab324360729
Ben <ben_chen@hengyuan.com.tw>
2025-07-23 15:18:29 +0800

    [Feat] keyword

--- app/src/main/java/com/awilab/moviedb/ui/search/SearchPage.kt
+++ app/src/main/java/com/awilab/moviedb/ui/search/SearchPage.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.awilab.moviedb.R
 import com.awilab.moviedb.common.navigation.MovieDbDestination
 import com.awilab.moviedb.ui.widgets.AppBar
@@ -17,21 +20,29 @@ import com.awilab.moviedb.ui.widgets.AppBar
 fun SearchPage(
     vm: SearchViewModel = hiltViewModel(),
 ) {
+    val keyword by vm.keyword.collectAsStateWithLifecycle()
+
     Scaffold(
         topBar = {
             AppBar(titleRes = R.string.app_name)
         },
         modifier = Modifier.fillMaxSize(),
     ) { innerPadding ->
-        Column(
+        Surface(
             modifier = Modifier
-                .padding(innerPadding)
-                .clickable {
-                },
+                .fillMaxSize()
+                .padding(innerPadding),
         ) {
-            Text(text = "Hello Search Screen", modifier = Modifier.clickable {
-                vm.navigator.navigate(MovieDbDestination.DetailDestination.route)
-            })
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .clickable {
+                    },
+            ) {
+                Text(text = "Hello Search Screen", modifier = Modifier.clickable {
+                    vm.navigator.navigate(MovieDbDestination.DetailDestination.route)
+                })
+            }
         }
     }
 }
--- app/src/main/java/com/awilab/moviedb/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/awilab/moviedb/ui/search/SearchViewModel.kt
@@ -3,11 +3,17 @@ package com.awilab.moviedb.ui.search
 import androidx.lifecycle.ViewModel
 import com.awilab.moviedb.common.navigation.MovieDbNavigator
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    val  navigator: MovieDbNavigator
+    val navigator: MovieDbNavigator
 ) : ViewModel() {
+    private val _keyword = MutableStateFlow("")
+    val keyword: StateFlow<String> = _keyword.asStateFlow()
+
 
 }
